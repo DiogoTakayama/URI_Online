4fd653e7dcef96935c9552f36b0bb55373f521d4
Werdeles Marcio de C. Soares <werdelesmarcio@hotmail.com>
2017-08-14 09:44:06 -0300

    Create 1008.c

---
+++ Iniciante/1008.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main ()
+{
+    int num_fun,hor_tra;float val_hor_tra;
+    
+    scanf("%i",&num_fun);
+    scanf("%i",&hor_tra);
+    scanf("%f",&val_hor_tra);
+
+    printf("NUMBER = %i\n",num_fun);
+
+    printf("SALARY = U$ %.2f\n",hor_tra*val_hor_tra);
+    
+    return 0;
+}
