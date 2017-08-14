bf23d62eb5b7679e155ea5234dc260f973fa0a8b
Werdeles Marcio de C. Soares <werdelesmarcio@hotmail.com>
2017-08-14 09:54:46 -0300

    Create 1115.c

---
+++ Iniciante/1115.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main()
+{
+    int x,y;
+
+
+    while(1)
+    {
+        scanf("%d",&x);
+        scanf("%d",&y);
+
+        if((x==0)||(y==0))
+        {
+            break;
+        }
+
+        if((x>0)&&(y>0))
+            printf("primeiro\n");
+        else
+        {
+            if((x<0)&&(y>0))
+                printf("segundo\n");
+            else
+            {
+                if((x<0)&&(y<0))
+                    printf("terceiro\n");
+                else
+                {
+                    if((x>0)&&(y<0))
+                    printf("quarto\n");
+                }
+            }
+        }
+    }
+    return 0;
+}
