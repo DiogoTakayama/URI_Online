bc6d4d21a504f01fd0a029b0f5b7fdc9d66d3195
Werdeles Marcio de C. Soares <werdelesmarcio@hotmail.com>
2017-08-14 09:46:03 -0300

    Create 1009.c

---
+++ Iniciante/1009.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    char nome[20];double Sal_Fix;double Tot_Ven;
+
+        scanf(" %s",&nome);
+        scanf("%lf",&Sal_Fix);
+        scanf("%lf",&Tot_Ven);
+        
+    printf("TOTAL = R$ %0.2lf\n",Sal_Fix + (Tot_Ven*0.15));
+    return 0;
+}
